450078cc0952aa7f9b14f72a54c9bd0951afe858
Mohammad Alwi Irfani <alwi.irfani1927@gmail.com>
2025-08-18 19:02:57 +0700

    [?] debug and fix process (1)

--- lib/notification.ts
+++ lib/notification.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
+const BASE_URL = process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000";
 
 // Kirim notifikasi ke beberapa user sekaligus
 export async function sendNotification(
